357fb1e3d95656eaba9e2076453f74ae2b07d928
Calavera39 <a.suturin@mail.ru>
2021-12-18 19:00:07 +0300

    second

---
+++ src/js/script.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-l.png"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-r.png"</button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+            }
+            }
+        ]
+      });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var name = "Ivan";
+let number = 7;
+const pi = 3.14;
+
+
+number = 4; */
+/* pi = 5;
+
+let leftBorderWidth = 200;  */
+
+// number - числа 
+// string - любая информация которую можно описать словами, нужны ковычки "" '' ``
+//  true/false 
+// null
+// undefined - существует но не имеет значения
+
+/* let obj =  {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+} */
+
+// Symbol
+
+/* alert(1234) */
+
+/* console.log(number) */
+/* let answer = confirm("вам есть 19?");
+console.log(answer) */
+
+/* let answer = prompt("вам есть 19?", "");
+console.log(answer); */
+
+/* console.log(4 + 'fdd'); */
+
+/* let isChecked = false,
+    isClose = false;
+
+/* console.log(isChecked && isClose) */
+
+/* console.log(isChecked || isClose); */ 
+
+
+/* if (2*1 == 8*1) {
+    console.log('Верно')
+} else {
+    console.log('ошибка')
+} */
+
+/* let answer = confirm("есть ли вам 18?");
+if (answer) {
+    console.log('Go')
+} else {
+    console.log('Go away')
+} */
+
+/* const num = 50;
+
+if (num < 49) {
+    console.log('not correct')
+} else if (num > 100) {
+    console.log ('many')
+} else {
+    console.log('correct')
+} */
+
+/* for(let i = 1; i<8; i++) {
+    console.log(i);
+} */
+
+/* function logging(a, b) {
+    console.log( a + b )
+}
+
+logging(3, 5);
+
+logging(6, 8); */
+

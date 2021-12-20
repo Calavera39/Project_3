3ae8d847fd9d9776c294133d09aa16f248bf345f
Calavera39 <a.suturin@mail.ru>
2021-12-20 18:03:26 +0300

    third

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         variableWidth: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-l.png"</button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-r.png"</button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 575,
                 settings: {
                     dots: true,
                     arrows: false
@@ -16,7 +16,51 @@ $(document).ready(function(){
             }
         ]
       });
-  });
+
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+
+
+
+      function toggleSlide(item) {
+          $(item).each(function(i) {
+            $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+
+    // modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+   
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
 
 
 
@@ -136,7 +180,7 @@ if (num < 49) {
     console.log( a + b )
 }
 
-logging(3, 5);
+/* logging(3, 5);
 
 logging(6, 8); */
 
